171e411dc19aa4181fdef5be0b6c91bfc74069f8
desmonddai583 <desmonddai583@gmail.com>
2018-02-01 18:49:11 +0800

    add flask migrate

--- services/users/manage.py
+++ services/users/manage.py
@@ -1,6 +1,7 @@
 import unittest
 
 from flask_script import Manager
+from flask_migrate import MigrateCommand
 import coverage
 
 from project import create_app, db
@@ -19,7 +20,7 @@ COV.start()
 
 app = create_app()
 manager = Manager(app)
-
+manager.add_command('db', MigrateCommand)
 
 @manager.command
 def test():
