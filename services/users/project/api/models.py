38c526bfbc894710f41fb12a927daf7ef5aa2dc0
desmonddai583 <desmonddai583@gmail.com>
2018-02-02 19:07:11 +0800

    modify bcrypt log rounds

--- services/users/project/api/models.py
+++ services/users/project/api/models.py
@@ -1,3 +1,4 @@
+from flask import current_app
 from project import db, bcrypt
 
 
@@ -12,7 +13,9 @@ class User(db.Model):
     def __init__(self, username, email, password):
         self.username = username
         self.email = email
-        self.password = bcrypt.generate_password_hash(password).decode()
+        self.password = bcrypt.generate_password_hash(
+            password, current_app.config.get('BCRYPT_LOG_ROUNDS')
+        ).decode()
 
     def to_json(self):
         return {
