d4ff2e348ceaddad9a2dcccce444e2b64d359f7c
Desmonddai583 <desmonddai583@gmail.com>
2018-02-10 15:54:39 +0800

    add auth route

--- services/users/project/api/auth.py
+++ services/users/project/api/auth.py
@@ -7,6 +7,7 @@ from project import db, bcrypt
 
 auth_blueprint = Blueprint('auth', __name__)
 
+
 @auth_blueprint.route('/auth/register', methods=['POST'])
 def register_user():
     # get post data
@@ -74,7 +75,7 @@ def login_user():
             response_object['message'] = 'User does not exist.'
             return jsonify(response_object), 404
     except Exception as e:
-        response_object['message'] = 'Try again.'     
+        response_object['message'] = 'Try again.'
         return jsonify(response_object), 500
 
 
@@ -98,3 +99,26 @@ def logout_user():
             return jsonify(response_object), 401
     else:
         return jsonify(response_object), 403
+
+
+@auth_blueprint.route('/auth/status', methods=['GET'])
+def get_user_status():
+    # get auth token
+    auth_header = request.headers.get('Authorization')
+    response_object = {
+        'status': 'fail',
+        'message': 'Provide a valid auth token.'
+    }
+    if auth_header:
+        auth_token = auth_header.split(' ')[1]
+        resp = User.decode_auth_token(auth_token)
+        if not isinstance(resp, str):
+            user = User.query.filter_by(id=resp).first()
+            response_object['status'] = 'success'
+            response_object['message'] = 'Success.'
+            response_object['data'] = user.to_json()
+            return jsonify(response_object), 200
+        response_object['message'] = resp
+        return jsonify(response_object), 401
+    else:
+        return jsonify(response_object), 401
--- services/users/project/tests/test_auth.py
+++ services/users/project/tests/test_auth.py
@@ -1,8 +1,6 @@
 import json
 from flask import current_app
 
-from project import db
-from project.api.models import User
 from project.tests.base import BaseTestCase
 from project.tests.utils import add_user
 
@@ -10,21 +8,21 @@ from project.tests.utils import add_user
 class TestAuthBlueprint(BaseTestCase):
     def test_user_registration(self):
         with self.client:
-              response = self.client.post(
+            response = self.client.post(
                   '/auth/register',
                   data=json.dumps({
-                      'username': 'justatest',
-                      'email': 'test@test.com',
-                      'password': '123456',
+                    'username': 'justatest',
+                    'email': 'test@test.com',
+                    'password': '123456',
                   }),
                   content_type='application/json'
-              )
-              data = json.loads(response.data.decode())
-              self.assertTrue(data['status'] == 'success')
-              self.assertTrue(data['message'] == 'Successfully registered.')
-              self.assertTrue(data['auth_token'])
-              self.assertTrue(response.content_type == 'application/json')
-              self.assertEqual(response.status_code, 201)
+            )
+            data = json.loads(response.data.decode())
+            self.assertTrue(data['status'] == 'success')
+            self.assertTrue(data['message'] == 'Successfully registered.')
+            self.assertTrue(data['auth_token'])
+            self.assertTrue(response.content_type == 'application/json')
+            self.assertEqual(response.status_code, 201)
 
     def test_user_registration_duplicate_email(self):
         add_user('test', 'test@test.com', 'test')
@@ -204,6 +202,41 @@ class TestAuthBlueprint(BaseTestCase):
         with self.client:
             response = self.client.get(
                 '/auth/logout',
+                headers={'Authorization': 'Bearer invalid'})
+            data = json.loads(response.data.decode())
+            self.assertTrue(data['status'] == 'fail')
+            self.assertTrue(
+                data['message'] == 'Invalid token. Please log in again.')
+            self.assertEqual(response.status_code, 401)
+
+    def test_user_status(self):
+        add_user('test', 'test@test.com', 'test')
+        with self.client:
+            resp_login = self.client.post(
+                '/auth/login',
+                data=json.dumps({
+                    'email': 'test@test.com',
+                    'password': 'test'
+                }),
+                content_type='application/json'
+            )
+            token = json.loads(resp_login.data.decode())['auth_token']
+            response = self.client.get(
+                '/auth/status',
+                headers={'Authorization': f'Bearer {token}'}
+            )
+            data = json.loads(response.data.decode())
+            self.assertTrue(data['status'] == 'success')
+            self.assertTrue(data['data'] is not None)
+            self.assertTrue(data['data']['username'] == 'test')
+            self.assertTrue(data['data']['email'] == 'test@test.com')
+            self.assertTrue(data['data']['active'] is True)
+            self.assertEqual(response.status_code, 200)
+
+    def test_invalid_status(self):
+        with self.client:
+            response = self.client.get(
+                '/auth/status',
                 headers={'Authorization': 'Bearer invalid'})
             data = json.loads(response.data.decode())
             self.assertTrue(data['status'] == 'fail')
